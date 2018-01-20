91d6bf3fe8582b9cda2c1a0d2a0065b470abb3d5
Jefferson Siqueira <jefferson08jsa@gmail.com>
2018-01-20 00:15:00 -0200

    gestaoFinanceira

---
+++ src/main/java/br/com/jsa/gestaoFinanceira/GestaoFinanceiraApplication.java
@@ -0,0 +1,12 @@
+package br.com.jsa.gestaoFinanceira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoFinanceiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoFinanceiraApplication.class, args);
+	}
+}
---
+++ src/main/java/br/com/jsa/gestaoFinanceira/controller/UsuarioController.java
@@ -0,0 +1,18 @@
+package br.com.jsa.gestaoFinanceira.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UsuarioController {
+
+	@GetMapping("/")
+	public String login() {
+		return "index";
+	}
+	
+	@GetMapping(value = "/home" )
+	public String home() {
+		return "home";
+	}
+}
