2f36e8a57344821661b803a0f8089815c4724f2b
Jefferson Siqueira <jefferson08jsa@gmail.com>
2018-01-20 00:39:38 -0200

    UsuarioController

--- src/main/java/br/com/jsa/gestaoFinanceira/controller/UsuarioController.java
+++ src/main/java/br/com/jsa/gestaoFinanceira/controller/UsuarioController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class UsuarioController {
 
 	@GetMapping("/")
-	public String login() {
+	public String login() { 
 		return "index";
 	}
 	
